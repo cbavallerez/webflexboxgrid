991b0182976e547ad971371b6d18980ea4c7bed2
cbavallerez <cbavallerez@gmail.com>
2017-04-09 19:43:15 -0300

    Primer commit

---
+++ js/header.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+
+	$( "li" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 500 );
+});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 200 ){
+			$('nav').height("50px");
+			$('nav').addClass('nav2');
+			$('.logo').height("50px");
+			$('nav li').addClass('li2');
+
+
+
+
+
+
+
+		} else {
+			$('nav').height("150px");
+			$('nav').removeClass('nav2');
+			$('.logo').height("100%");
+			$('nav li').removeClass('li2');
+
+
+			$('.headersection').removeClass('headersection2');
+			$('.subtitulo').show();
+
+
+
+		}
+	});
+
+});
